a2a64bf915f2fcb2dc45919c85e353153f448738
shuvro111 <iamshuvrosarkar@gmail.com>
2023-08-21 00:46:32 +0600

    fix auth issues

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="w-full h-full py-16  min-h-screen flex justify-center items-center">
+      <SignIn
+        signUpUrl="/sign-up"
+        appearance={{
+          baseTheme: dark,
+          variables: {
+            colorPrimary: "rgb(52 211 153)",
+            colorText: "rgb(255 255 255)",
+            colorBackground: "rgb(15 23 42)",
+            colorInputBackground: "rgb(15 23 42)",
+            colorTextOnPrimaryBackground: "rgb(2 6 23)",
+          },
+        }}
+      />
+    </div>
+  );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,23 @@
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="w-full h-full py-16  min-h-screen flex justify-center items-center">
+      <SignUp
+        signInUrl="/sign-in"
+        afterSignUpUrl="/onboarding"
+        appearance={{
+          baseTheme: dark,
+          variables: {
+            colorPrimary: "rgb(52 211 153)",
+            colorText: "rgb(255 255 255)",
+            colorBackground: "rgb(15 23 42)",
+            colorInputBackground: "rgb(15 23 42)",
+            colorTextOnPrimaryBackground: "rgb(2 6 23)",
+          },
+        }}
+      />
+    </div>
+  );
 }
