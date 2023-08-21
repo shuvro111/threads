dee3bf53cd910a8bab449ecadb2e37e356b705d3
shuvro111 <iamshuvrosarkar@gmail.com>
2023-08-22 00:55:24 +0600

    fixed and added few shared comopnents

--- src/components/shared/Feed.tsx
+++ src/components/shared/Feed.tsx
@@ -1,27 +1,58 @@
 import { getThreads } from "@/lib/actions/thread.actions";
 import ThreadCard from "@/components/cards/ThreadCard";
+import RepostCard from "@/components/cards/RepostCard";
 import { User } from "@/lib/types/user";
 import { currentUser } from "@clerk/nextjs";
+import { ThreadCard as ThreadCardType } from "@/lib/types/thread";
 
 export default async function Feed() {
   const user = await currentUser();
   if (!user) return null;
 
   const threads = await getThreads();
+
   return (
     <div className="flex flex-col gap-12">
-      {threads.map((thread) => (
-        <ThreadCard
-          key={thread.id}
-          id={thread.id}
-          author={thread.author as User}
-          communityId={thread.communityId}
-          content={String(thread.content)}
-          createdAt={thread.createdAt}
-          media={String(thread.media)}
-          currentUserId={String(user.id)}
-        />
-      ))}
+      {threads.map((thread) => {
+        const parent: any = {};
+        if (thread.isRepost && thread.parent) {
+          parent.id = thread.parent.id;
+          parent.author = thread.parent.author;
+          parent.likedBy = thread.parent.likedBy;
+          parent.communityId = thread.parent.communityId;
+          parent.content = thread.parent.content;
+          parent.media = thread.parent.media;
+          parent.createdAt = thread.parent.createdAt;
+          parent.currentUserId = user.id;
+        }
+
+        return thread.isRepost ? (
+          <RepostCard
+            key={thread.id}
+            id={thread.id}
+            author={thread.author as User}
+            likedBy={thread.likedBy as User[]}
+            communityId={thread.communityId}
+            content={String(thread.content)}
+            createdAt={thread.createdAt}
+            media={String(thread.media)}
+            currentUserId={String(user.id)}
+            parent={parent}
+          />
+        ) : (
+          <ThreadCard
+            key={thread.id}
+            id={thread.id}
+            author={thread.author as User}
+            likedBy={thread.likedBy as User[]}
+            communityId={thread.communityId}
+            content={String(thread.content)}
+            createdAt={thread.createdAt}
+            media={String(thread.media)}
+            currentUserId={String(user.id)}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -1,8 +1,27 @@
-export default function RightSidebar() {
+import { getSuggestedUsers } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+import SuggestedUserCard from "@/components/cards/SuggestedUserCard";
+
+export default async function RightSidebar() {
+  const user = await currentUser();
+  const suggestedUsers = await getSuggestedUsers(String(user?.id));
+
   return (
-    <section className="hidden w-1/4 xl:flex xl:flex-col sticky top-0 right-0 h-screen overflow-auto bg-slate-900 text-white justify-start pt-[75px] px-12">
-      <h1 className="text-xl font-semibold h-80">Suggested Accounts</h1>
-      <h1 className="text-xl font-semibold">Similar Minds</h1>
+    <section className="hidden w-1/4 xl:flex xl:flex-col gap-4 sticky top-0 right-0 h-screen overflow-auto bg-slate-900 text-white justify-start pt-[75px] px-12">
+      <h1 className="text-xl font-semibold">Suggested Accounts</h1>
+      <div className="flex flex-col gap-4">
+        {suggestedUsers?.map((user) => (
+          <SuggestedUserCard
+            key={user.id}
+            userId={user.id}
+            name={user.name}
+            username={user.username}
+            avatar={String(user.avatar)}
+          />
+        ))}
+      </div>
+      <h1 className="text-xl font-semibold mt-8">Similar Minds</h1>
+      <p className="text-lg text-slate-400">Coming Soon...</p>
     </section>
   );
 }
