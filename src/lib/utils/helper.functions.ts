25b09517390adb3f84d57089ac64ed2a33a57e3b
shuvro111 <iamshuvrosarkar@gmail.com>
2023-08-28 01:09:13 +0600

    made some minor changes & ready for deployment

--- src/lib/utils/helper.functions.ts
+++ src/lib/utils/helper.functions.ts
@@ -1,6 +1,7 @@
 import { currentUser } from "@clerk/nextjs";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { User } from "../types/user";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,4 +10,8 @@ export function cn(...inputs: ClassValue[]) {
 export function isBase64Image(imageData: string) {
   //check if string is base64 encoded image with regex
   return imageData.match(/^data:image\/(png|jpg|jpeg|gif|webp);base64,/);
+}
+
+export function getIsLikedByUser(likedBy: User[], userId: string) {
+  return likedBy.some((like) => like.id === userId);
 }
